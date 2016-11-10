bb11a20af647352a3b229c6dabfc643cd01ba96c
Ricardo Vazquez <vazquezmjr@gmail.com>
2016-11-10 09:48:01 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,4 @@
+var sum= require("./sumLibrary");
+console.log("Display information");
+console.log(sum.add(5,9));
+console.log(sum.textAdd("textOne","textTwo"));
---
+++ sumLibrary.js
@@ -0,0 +1,6 @@
+exports.add=function(a,b){
+	return parseInt(a+b);
+}
+exports.textAdd=function(a,b){
+	return a+b;
+}
